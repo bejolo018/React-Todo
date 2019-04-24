ec36620e4f337e261ed4204415a3584e6d3e12ca
unknown <bejolo018@gmail.com>
2019-04-24 17:34:50 -0500

    added functionality to form

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,11 @@ import React from 'react';
 const TodoForm = props => {
     return (
         <form>
-            
+            <input 
+            placeholder='...todo'
+            onChange={props.handleTodoChange}
+            value={props.value}
+            />
             <button> Add Todo</button>
             <button> Clear Completed</button>
         </form>
